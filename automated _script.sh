52e409c91bac3cd229e46aebf72cf9d5e633a822
astra <26215963+3tternp@users.noreply.github.com>
2024-04-11 13:33:23 +0545

    Update automated _script.sh

--- automated _script.sh
+++ automated _script.sh
@@ -38,116 +38,116 @@ do
     case $count in
         1)
             read -p "Enter IP address: " ip
-            nmap -sU -p 53 --script dns-cache-snoop.nse --script-args 'dns-cache-snoop.mode=timed' "$ip"
+            sudo nmap -sU -p 53 --script dns-cache-snoop.nse --script-args 'dns-cache-snoop.mode=timed' "$ip"
             ;;
         2)
             read -p "Enter port: " port
             read -p "Enter IP address: " IP
-            nmap -sV --version-light --script ssl-poodle -p "$port" "$IP"
+            sudo nmap -sV --version-light --script ssl-poodle -p "$port" "$IP"
             ;;
         3)
             read -p "Enter port: " port
             read -p "Enter IP address: " IP
-            nmap -sV --script ssl-enum-ciphers -p "$port" "$IP"
+            sudo nmap -sV --script ssl-enum-ciphers -p "$port" "$IP"
             ;;
         4)
             read -p "Enter IP address: " IP
-            nmap -sV --script=sslv2-drown "$IP"
+            sudo nmap -sV --script=sslv2-drown "$IP"
             ;;
         5)
             read -p "Enter IP address: " IP
-            nmap -sV -sC "$IP"
+            sudo nmap -sV -sC "$IP"
             ;;
         6)
             read -p "Enter IP address: " IP
-            nmap --script smb-security-mode.nse -p 445 "$IP"
+            sudo nmap --script smb-security-mode.nse -p 445 "$IP"
             ;;
         7)
             read -p "Enter IP address: " IP
-            nmap --script smb-os-discovery.nse -p 445 "$IP"
+            sudo nmap --script smb-os-discovery.nse -p 445 "$IP"
             ;;
         8)
             read -p "Enter IP address: " IP
-            nmap -sn "$IP" --script ip-forwarding --script-args='target=www.google.com'
+            sudo nmap -sn "$IP" --script ip-forwarding --script-args='target=www.google.com'
             ;;
         9)
             read -p "Enter IP address: " IP
-            nmap --script=mysql-enum "$IP"
+            sudo nmap --script=mysql-enum "$IP"
             ;;
         10)
             read -p "Enter IP address: " IP
-            nmap -p 512 --script rexec-brute "$IP"
+            sudo nmap -p 512 --script rexec-brute "$IP"
             ;;
         11)
             read -p "Enter IP address: " IP
-            nmap -p 513 --script rlogin-brute "$IP"
+            sudo nmap -p 513 --script rlogin-brute "$IP"
             ;;
         12)
             read -p "Enter port: " port
             read -p "Enter IP address: " IP
-            nmap --script firewall-bypass "$port" "$IP"
+            sudo nmap --script firewall-bypass "$port" "$IP"
             ;;
         13)
             read -p "Enter IP address: " IP
-            nmap --script smb-vuln-conficker.nse -p 445 "$IP"
+            sudo nmap --script smb-vuln-conficker.nse -p 445 "$IP"
             ;;
         14)
             read -p "Enter IP address: " IP
-            nmap -p 27017 --script mongodb-info "$IP"
+            sudo nmap -p 27017 --script mongodb-info "$IP"
             ;;
         15)
             read -p "Enter IP address: " IP
-            nmap -p 1433 --script ms-sql-info --script-args mssql.instance-port=1433 "$IP"
+            sudo nmap -p 1433 --script ms-sql-info --script-args mssql.instance-port=1433 "$IP"
             ;;
         16)
             read -p "Enter port: " port
             read -p "Enter IP address: " IP
-            nmap --script smtp-enum-users.nse -p "$port" "$IP"
+            sudo nmap --script smtp-enum-users.nse -p "$port" "$IP"
             ;;
         17)
             read -p "Enter IP address: " IP
-            nmap -p 3389 --script rdp-enum-encryption "$IP"
+            sudo nmap -p 3389 --script rdp-enum-encryption "$IP"
             ;;
         18)
             read -p "Enter IP address: " IP
-            nmap --script smb-system-info.nse -p 445 "$IP"
+            sudo nmap --script smb-system-info.nse -p 445 "$IP"
             ;;
         19)
             read -p "Enter IP address: " IP
-            nmap -p 1433 --script ms-sql-config --script-args mssql.username=sa,mssql.password=admin123 "$IP"
+            sudo nmap -p 1433 --script ms-sql-config --script-args mssql.username=sa,mssql.password=admin123 "$IP"
             ;;
         20)
             read -p "Enter IP address: " IP
-            nmap -p 1433 --script ms-sql-dump-hashes --script-args mssql.username=sa,mssql.password=admin123 "$IP"
+            sudo nmap -p 1433 --script ms-sql-dump-hashes --script-args mssql.username=sa,mssql.password=admin123 "$IP"
             ;;
         21)
             read -p "Enter IP address: " IP
-            smbmap -u anonymous -p anonymous -d WORKGROUP -H "$IP"
+            sudo smbmap -u anonymous -p anonymous -d WORKGROUP -H "$IP"
             ;;
         22)
             read -p "Enter IP address: " IP
-            nmap -sU -p 123 --script ntp-info "$IP"
+            sudo nmap -sU -p 123 --script ntp-info "$IP"
             ;;
         23)
             read -p "Enter IP address: " IP
-            nmap -sU -pU:123 -Pn -n --script=ntp-monlist "$IP"
+            sudo nmap -sU -pU:123 -Pn -n --script=ntp-monlist "$IP"
             ;;
         24)
             read -p "Enter port: " port
             read -p "Enter IP address: " IP
-            nmap --script=oracle-tns-poison -p "$port" "$IP"
+            sudo nmap --script=oracle-tns-poison -p "$port" "$IP"
             ;;
         25)
             read -p "Enter IP address: " ip
-            nmap -sU -p 177 --script xdmcp-discover "$ip"
+            sudo nmap -sU -p 177 --script xdmcp-discover "$ip"
             ;;
         26)
             read -p "Enter IP address: " ip
-            nmap -Pn -p 22 --script ssh2-enum-algos "$ip"
+            sudo nmap -Pn -p 22 --script ssh2-enum-algos "$ip"
             ;;
         27)
             read -p "Enter IP address: " ip
-            nmap -sV --script vulners "$ip"
+            sudo nmap -sV --script vulners "$ip"
             ;;
         *)
             echo "You are out of stock"
